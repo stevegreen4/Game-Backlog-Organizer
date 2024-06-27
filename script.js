1a0a85b1b30dc4f236724b936c1ec089ef7f84c6
Steve Green <sgreen@spectrummachine.com>
2024-06-27 13:30:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Create a search bar that pulls games from RAWG API 
+// Allow users to add games to backlog list 
+// have checkbox for completed. Move completed game to section below showcasing all completed games. 
+// Create a random button to randomly pick a game in their list. 
+require('dotenv').config();
+const API_KEY = process.env.API_KEY;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('search-bar')
+    .addEventListener('input', function() {
+        let input = String(this.value);
+        if(input.length > 2) {
+            fetch('https://api.rawg.io/api/games?key=' + API_KEY + `&search=${input}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                displaySearchResults(data.results);
+            })
+        }
+    })
+});
+
+function displaySearchResults(results) {
+    const resultsContainer = document.getElementById('search-list');
+    resultsContainer.innerHTML = '';
+
+    results.forEach(game => {
+        const listItem = document.createElement('li');
+        listItem.className = 'result-item';
+        listItem.textContent = game.name;
+        resultsContainer.appendChild(listItem);
+    });
+}
+
